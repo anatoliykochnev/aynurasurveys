2d1d2a3d67b6da4e608c2eed74adcf3bf7de31f8
ChronifyAI <anatoliy.kochnev@gmail.com>
2026-04-27 12:27:34 -0400

    update

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_aynurasurveys';
-$plugin->version      = 2026042417;
+$plugin->version      = 2026042418;
 $plugin->requires     = 2024100700;
 $plugin->maturity     = MATURITY_STABLE;
-$plugin->release      = '2.1.4';
+$plugin->release      = '2.1.5';
